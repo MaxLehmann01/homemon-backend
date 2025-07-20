3e4932ef46c2c26ed45f140d82db84c59138167e
Max Lehmann <85064456+MaxLehmann01@users.noreply.github.com>
2025-07-20 11:55:47 +0200

    Added: jest setup

---
+++ jest.config.ts
@@ -0,0 +1,22 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testRegex: '.*\\.spec\\.ts$',
+    collectCoverage: true,
+    collectCoverageFrom: ['src/**/*.ts'],
+    coveragePathIgnorePatterns: ['/node_modules/', './src/config/Schema.ts'],
+    coverageReporters: ['text', 'cobertura'],
+    coverageDirectory: './coverage',
+    transform: {
+        '^.+\\.tsx?$': [
+            'ts-jest',
+            {
+                tsconfig: 'tsconfig.json',
+            },
+        ],
+    },
+    moduleNameMapper: {
+        '^src/(.*)$': '<rootDir>/src/$1',
+    },
+    setupFiles: ['tsconfig-paths/register'],
+};
